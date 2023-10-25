c8891adc74df2a2ee4cfa7bfda61a28a0ee8af61
meesh1544 <meesh1544@gmail.com>
2023-10-25 10:55:07 -0400

    1

--- Models/SpaContext.cs
+++ Models/SpaContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using RazorPagesZenSpaCh7.Models;
 
 namespace RazorPages.Models
 {
@@ -10,9 +11,22 @@ namespace RazorPages.Models
         public DbSet<Contact> Contacts { get; set; }
 
         public DbSet<Services> Services { get; set; }
+        public DbSet<Client> Clients { get; set; }
 
+        public DbSet<ClientService> ClientServices { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Client>().HasData(
+            new Client { ID = 1, FirstName = "Flo", LastName = "Schmoe", Username = "Flo", Email = "flo@schmoe.net", Password = "FloSchmoe1234*" },
+            new Client { ID = 2, FirstName = "Jo", LastName = "Schmoe", Username = "JoJo", Email = "jojo@schmoe.net", Password = "JoJoSchmoe1234?" },
+            new Client { ID = 3, FirstName = "Truly", LastName = "Schmoe", Username = "Truly", Email = "truly@schmoe.net", Password = "Truly9876**" }
+            );
+            modelBuilder.Entity<ClientService>().HasData(
+            new ClientService { ID = 1, ClientID = 1, ServicesID = 1 },
+            new ClientService { ID = 2, ClientID = 2, ServicesID = 7 },
+            new ClientService { ID = 3, ClientID = 1, ServicesID = 5 }
+            );
+
             modelBuilder.Entity<Services>().HasData(
                 new Services { ID = 1, Name = "Full-Day Treatment Package", Classification = "Full", Fee = 450 },
                 new Services { ID = 2, Name = "Half-Day Treatment Package", Classification = "Half", Fee = 300 },
